ade34e0a7051a93358908e1527db32650d77612c
Indra Zulfi <me@indrazm.com>
2023-10-21 19:37:55 +0700

    chore(test): prisma fetching test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
+import prisma from '@/utils/prisma/prisma';
+
+async function getConfig() {
+  const config = await prisma.config.findFirst();
+  return config;
+}
+
+export default async function Page() {
+  const data = await getConfig();
+  console.log(data);
+
   return <main>Hey</main>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      primary: {
+        50: '#f4f3ff',
+        100: '#ebe9fe',
+        200: '#d8d6fe',
+        300: '#bcb5fd',
+        400: '#9a8bfa',
+        500: '#795df5',
+        600: '#673bec',
+        700: '#5829d8',
+        800: '#441fa7',
+        900: '#3e1e94',
+        950: '#241065',
+      },
+      secondary: {
+        50: '#f6f6f6',
+        100: '#e7e7e7',
+        200: '#d1d1d1',
+        300: '#b0b0b0',
+        400: '#888888',
+        500: '#6d6d6d',
+        600: '#5d5d5d',
+        700: '#4f4f4f',
+        800: '#454545',
+        900: '#3d3d3d',
+        950: '#0e0e0e',
       },
     },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
